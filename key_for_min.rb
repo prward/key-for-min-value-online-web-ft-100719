3cd755a42184bb3fae3bde3064105ad6e68d52ae
Patrick Ward <patrick.r.ward@verizon.net>
2019-10-10 16:15:30 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,8 +3,10 @@
 
 def key_for_min_value(name_hash)
   smallest_value = 0
+  smallest_key = nil
   name_hash.each do |key, value|
     if smallest_value == 0 || value < smallest_value
-      
+      smallest_value = value
+      smallest_key = key
 
 end
